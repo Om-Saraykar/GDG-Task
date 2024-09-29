538584099b52bda2606f5aff3e8e6e88d7a3f362
Om-Saraykar <omsaraykar@gmail.com>
2024-09-29 09:48:08 +0530

    Docker added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './components/Home';
 import './index.css'; // Import the Tailwind CSS file
 
